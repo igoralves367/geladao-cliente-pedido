50d53a383b3e20a7adaa1012f4ce2a18b644903c
igoralves367 <igor.alves367@gmail.com>
2022-11-13 19:30:18 -0300

    Implementa API, Teste Ok

--- src/main/java/br/com/casadogeladao/geladaoclientepedido/GeladaoClientePedidoApplication.java
+++ src/main/java/br/com/casadogeladao/geladaoclientepedido/GeladaoClientePedidoApplication.java
@@ -2,9 +2,19 @@ package br.com.casadogeladao.geladaoclientepedido;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@RequestMapping("/")
 public class GeladaoClientePedidoApplication {
+	
+	@GetMapping
+	public String getHomeTeste () {
+		return "Geladao Cliente Pedido - Novo API Home";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(GeladaoClientePedidoApplication.class, args);

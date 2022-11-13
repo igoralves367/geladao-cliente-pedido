de0b5b572ad4e6e663e9b19cdcc0fd37d129cff6
igoralves367 <igor.alves367@gmail.com>
2022-11-13 19:12:17 -0300

    inicia projeto

---
+++ src/main/java/br/com/casadogeladao/geladaoclientepedido/GeladaoClientePedidoApplication.java
@@ -0,0 +1,13 @@
+package br.com.casadogeladao.geladaoclientepedido;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeladaoClientePedidoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeladaoClientePedidoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/casadogeladao/geladaoclientepedido/GeladaoClientePedidoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.casadogeladao.geladaoclientepedido;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeladaoClientePedidoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

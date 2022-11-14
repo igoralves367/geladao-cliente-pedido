5d199cd5d40e7b320b9aa24eb14abb873ff256c0
igoralves367 <igor.alves367@gmail.com>
2022-11-14 18:52:05 -0300

    ajuste do contrato da API

--- src/main/java/br/com/casadogeladao/geladaoclientepedido/cliente/application/api/ClienteAPI.java
+++ src/main/java/br/com/casadogeladao/geladaoclientepedido/cliente/application/api/ClienteAPI.java
@@ -13,5 +13,4 @@ public interface ClienteAPI {
 	@PostMapping
 	@ResponseStatus(code = HttpStatus.CREATED)
 	ClienteResponse postCliente(@RequestBody ClienteRequest clienteRequest);
-
 }
--- src/main/java/br/com/casadogeladao/geladaoclientepedido/cliente/application/api/ClienteController.java
+++ src/main/java/br/com/casadogeladao/geladaoclientepedido/cliente/application/api/ClienteController.java
@@ -1,7 +1,6 @@
 package br.com.casadogeladao.geladaoclientepedido.cliente.application.api;
 
 import org.springframework.web.bind.annotation.RestController;
-
 import lombok.extern.log4j.Log4j2;
 
 @RestController

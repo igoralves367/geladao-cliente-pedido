0fce364278d8ef132bb92814fb3e0440f3deaaac
igoralves367 <igor.alves367@gmail.com>
2022-11-16 17:20:03 -0300

    implementa cliente respository teste ok

--- src/main/java/br/com/casadogeladao/geladaoclientepedido/cliente/infra/ClienteInfraRepository.java
+++ src/main/java/br/com/casadogeladao/geladaoclientepedido/cliente/infra/ClienteInfraRepository.java
@@ -1,20 +1,22 @@
 package br.com.casadogeladao.geladaoclientepedido.cliente.infra;
 
 import org.springframework.stereotype.Repository;
-
 import br.com.casadogeladao.geladaoclientepedido.cliente.application.repository.ClienteRepository;
 import br.com.casadogeladao.geladaoclientepedido.cliente.domain.Cliente;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 
 @Repository
 @Log4j2
+@RequiredArgsConstructor
 public class ClienteInfraRepository implements ClienteRepository {
+	private final ClienteSpringDataJPARepository clienteSpringDataJPARepository;
 
 	@Override
 	public Cliente salva(Cliente cliente) {
 		log.info("[inicia] ClienteInfraRepository - salva");
+		clienteSpringDataJPARepository.save(cliente);
 		log.info("[finaliza] ClienteInfraRepository - salva");
 		return cliente;
 	}
-
 }

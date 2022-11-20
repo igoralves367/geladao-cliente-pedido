b391960969086521442a4c483bce370dd1da841a
igoralves367 <igor.alves367@gmail.com>
2022-11-20 11:07:15 -0300

    implementa pedido domain

--- src/main/java/br/com/casadogeladao/geladaoclientepedido/cliente/domain/Cliente.java
+++ src/main/java/br/com/casadogeladao/geladaoclientepedido/cliente/domain/Cliente.java
@@ -5,6 +5,8 @@ import java.time.LocalDateTime;
 import java.util.UUID;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -36,6 +38,7 @@ public class Cliente {
 	@NotBlank
 	private String celular;
 	private String telefone;
+	@Enumerated(EnumType.STRING)
 	private Sexo sexo;
 	@NotNull
 	private LocalDate dataNascimento;
---
+++ src/main/java/br/com/casadogeladao/geladaoclientepedido/pedido/domain/Pedido.java
@@ -0,0 +1,68 @@
+package br.com.casadogeladao.geladaoclientepedido.pedido.domain;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@Getter
+@Entity
+public class Pedido {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(columnDefinition = "uuid", name = "idPedido", updatable = false, unique = true, nullable = false)
+	private UUID idPedido;
+	@NotNull
+	@Column(columnDefinition = "uuid", name = "idCliente", nullable = false)
+	private UUID idCliente;
+	@NotBlank
+	@Enumerated(EnumType.STRING)
+	private Sabor sabor;
+	private int quantidade;
+
+	private LocalDateTime dataHoraDoCadastro;
+	private LocalDateTime dataHoraDaUltimaAlteracao;
+	
+//	public Pet(UUID idCliente, @Valid PetRequest petRequest) {
+//		this.idClienteTutor = idCliente;
+//		this.nomePet = petRequest.getNomePet();
+//		this.porte = petRequest.getPorte();
+//		this.tipo = petRequest.getTipo();
+//		this.microchip = petRequest.getMicrochip();
+//		this.raca = petRequest.getRaca();
+//		this.sexo = petRequest.getSexo();
+//		this.pelagemCor = petRequest.getPelagemCor();
+//		this.dataNascimento = petRequest.getDataNascimento();
+//		this.rga = petRequest.getRga();
+//		this.peso = petRequest.getPeso();
+//		this.dataHoraDoCadastro = LocalDateTime.now();
+//	}
+//
+//	public void altera(PetAlteracaoRequest petRequest) {
+//		this.nomePet = petRequest.getNomePet();
+//		this.porte = petRequest.getPorte();
+//		this.tipo = petRequest.getTipo();
+//		this.microchip = petRequest.getMicrochip();
+//		this.raca = petRequest.getRaca();
+//		this.sexo = petRequest.getSexo();
+//		this.pelagemCor = petRequest.getPelagemCor();
+//		this.dataNascimento = petRequest.getDataNascimento();
+//		this.rga = petRequest.getRga();
+//		this.peso = petRequest.getPeso();
+//		this.dataHoraDaUltimaAlteracao = LocalDateTime.now();	
+//	}
+}
+//definimos o modelo de negocio, dominio e construtor
